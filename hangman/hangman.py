3c546793999d38b947db38f28ff225f5faaa316b
luke-who <33724568+luke-who@users.noreply.github.com>
2025-03-18 18:04:00 +0000

    Update Hangman game to display current word state and add emojis to win/lose messages

--- hangman/hangman.py
+++ hangman/hangman.py
@@ -1,44 +1,15 @@
 import random
 
-
 class Hangman:
-    """
-    A class to represent the Hangman game.
-
-    Attributes:
-    word (str): The word to be guessed in the game.
-    word_guessed (list): A list representing the current state of guessed letters in the word.
-    num_letters (int): The number of unique letters in the word that need to be guessed.
-    num_lives (int): The number of incorrect guesses allowed before the game is over.
-    word_list (list): A list of possible words to choose from for the game.
-    list_of_guesses (list): A list of letters that have already been guessed by the player.
-    """
-
     def __init__(self, word_list, num_lives=5):
-        """
-        Initializes a new instance of the Hangman game.
-
-        Parameters:
-        word_list (list): A list of words from which the game will randomly select one.
-        num_lives (int): The number of incorrect guesses allowed before the game is over. Default is 5.
-        """
         self.word = random.choice(word_list)
-        self.word_guessed = ["_"] * len(self.word)
+        self.word_guessed = ['_'] * len(self.word)
         self.num_letters = len(set(self.word))
         self.num_lives = num_lives
         self.word_list = word_list
         self.list_of_guesses = []
 
     def check_guess(self, guess):
-        """
-        Checks the player's guess against the selected word.
-
-        Parameters:
-        guess (str): The letter guessed by the player.
-
-        Returns:
-        None: This method modifies the state of the game (updates lives and guessed letters) but does not return a value.
-        """
         guess = guess.lower()
         if guess in self.word:
             print(f"Good guess! {guess} is in the word.")
@@ -46,21 +17,14 @@ class Hangman:
                 if letter == guess:
                     self.word_guessed[i] = guess
             self.num_letters -= 1
+            print('Current word:', ' '.join(self.word_guessed))
         else:
             self.num_lives -= 1
             print(f"Sorry, {guess} is not in the word.")
             print(f"You have {self.num_lives} lives left.")
+            print('Current word:', ' '.join(self.word_guessed))
 
     def ask_for_input(self):
-        """
-        Prompts the player to input a letter guess and validates the input.
-
-        This method continues to ask for input until a valid guess is made. It checks if the guess is a single alphabetical character and if it has not been guessed before.
-        If the guess is valid, it calls the check_guess method.
-
-        Returns:
-        None: This method does not return a value but modifies the state of the game based on user input.
-        """
         guess = input("Please guess a letter: ")
         if len(guess) != 1 or not guess.isalpha():
             print("Invalid letter. Please, enter a single alphabetical character.")
@@ -70,32 +34,19 @@ class Hangman:
             self.check_guess(guess)
             self.list_of_guesses.append(guess)
 
-
 def play_game(word_list):
-    """
-    Plays the Hangman game.
-
-    This function initializes a Hangman game instance and manages the game loop, checking for win/loss conditions.
-
-    Parameters:
-    word_list (list): A list of words from which the game will randomly select one.
-
-    Returns:
-    None: This function does not return a value but contains the logic to play the game.
-    """
     num_lives = 5
     game = Hangman(word_list, num_lives)
 
     while True:
         if game.num_lives == 0:
-            print("You lost!")
+            print("You lost! 💀")
             break
         elif game.num_letters > 0:
             game.ask_for_input()
         else:
-            print("Congratulations. You won the game!")
+            print("Congratulations. You won the game! 🎉")
             break
-
 
 if __name__ == "__main__":
     word_list = ["apple", "jackfruit", "blueberry", "pomegranate", "watermelon"]
